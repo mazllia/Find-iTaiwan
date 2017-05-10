f0bfc11b9d38549b12057d0b10c47ceb6874a5fe
戴鵬洋 <daipengyang.cs99@g2.nctu.edu.tw>
2017-05-10 16:20:15 +0800

    Simplify VC: remove FetchedResultsController and adopt array of MOs

--- Find iTaiwan/ViewController.swift
+++ Find iTaiwan/ViewController.swift
@@ -26,33 +26,15 @@ class ViewController: UIViewController {
 	var api: API = API.default
 	
 	// MARK: FetchedResultsController
-	fileprivate(set) var resultsController: NSFetchedResultsController<Location>!
-	func setupResultsControllerAndUpdateAnnotation(in map: MKMapView) {
-		let previousLocations = resultsController?.fetchedObjects ?? []
+	fileprivate(set) var locations = [Location]()
+	func updateLocationAndAnnotation(in map: MKMapView) {
+		let previousLocations = locations
 		let didShowAnnotaions = !map.annotations.isEmpty
 		
-		resultsController = {
-			let request: NSFetchRequest<Location> = Location.fetchRequest(in: map.region)
-			
-			// Arbitrary sort descriptor as we don't leverage on such feature of NSFetchedResultsController
-			request.sortDescriptors = [NSSortDescriptor(key: "latitude", ascending: true)]
-			
-			
-			let $ = NSFetchedResultsController(
-				fetchRequest: request,
-				managedObjectContext: api.modelContainer.viewContext,
-				sectionNameKeyPath: nil,
-				cacheName: nil
-			)
-			$.delegate = self
-			return $
-		}()
-		try! resultsController.performFetch()
-		
-		assert(resultsController.fetchedObjects != nil)
-		
-		let locations = resultsController.fetchedObjects ?? []
-		
+		locations = try! api.modelContainer.viewContext.fetch(
+			Location.fetchRequest(in: map.region)
+		)
+				
 		let shouldShowAnnotations: Bool = {
 			let $ = annotationVisibleThreshold
 			return map.camera.altitude < $.cameraDistance ||
@@ -102,42 +84,15 @@ class ViewController: UIViewController {
 		requestLocationAuthorization()
 		
 		mapView.setUserTrackingMode(.follow, animated: false)
+		
 	}
 }
 
 // MARK: - Delegates
-// MARK: FetchedResultsController
-extension ViewController: NSFetchedResultsControllerDelegate {
-	func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
-	                didChange anObject: Any,
-	                at indexPath: IndexPath?,
-	                for type: NSFetchedResultsChangeType,
-	                newIndexPath: IndexPath?) {
-		
-		switch type {
-		case .insert:
-			let $ = resultsController.object(at: newIndexPath!)
-			mapView.addAnnotation($)
-			
-		case .delete:
-			let $ = resultsController.object(at: indexPath!)
-			mapView.removeAnnotation($)
-			
-		case .update:
-			// No need to command any refresh as MapKit uses KVO in MKAnnotation
-			// - seealso: `MKAnnotationCalloutInfoDidChange` supports legacy applications and is no longer necessary. MapKit tracks changes to the title and subtitle of an annotation using KVO notifications
-			break
-			
-		case .move:
-			break
-		}
-	}
-}
-
 // MARK: Map View
 extension ViewController: MKMapViewDelegate {
 	func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-		 setupResultsControllerAndUpdateAnnotation(in: mapView)
+		 updateLocationAndAnnotation(in: mapView)
 	}
 }
 
