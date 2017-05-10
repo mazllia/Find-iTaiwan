87f280ef29e7884d04bf8ce5bd57745f42a61b9b
戴鵬洋 <daipengyang.cs99@g2.nctu.edu.tw>
2017-05-10 15:39:18 +0800

    Improve UX with annotationVisibleThreshold

--- Find iTaiwan/ViewController.swift
+++ Find iTaiwan/ViewController.swift
@@ -4,6 +4,22 @@ import MapKit
 import CoreData
 
 class ViewController: UIViewController {
+	/**
+	To prevent too many annotations showing in map, this threshold provides threshold guidance depends on current _size classes_.
+	- cameraDistance: if map.camera.altitude is smaller than this, show annotations
+	- annotationCount: if matched annotations are smaller than this, show annotations
+	*/
+	internal var annotationVisibleThreshold: (cameraDistance: CLLocationDistance, annotationCount: Int) {
+		switch (traitCollection.horizontalSizeClass, traitCollection.verticalSizeClass) {
+		case (.regular , .regular):
+			return (100000, 1200)
+		case (.compact, _), (_, .compact):
+			return (60000, 500)
+		default:
+			return (60000, 500)
+		}
+	}
+	
 	@IBOutlet weak var mapView: MKMapView!
 	let locationManager = CLLocationManager()
 	
@@ -13,6 +29,7 @@ class ViewController: UIViewController {
 	fileprivate(set) var resultsController: NSFetchedResultsController<Location>!
 	func setupResultsControllerAndUpdateAnnotation(in map: MKMapView) {
 		let previousLocations = resultsController?.fetchedObjects ?? []
+		let didShowAnnotaions = !map.annotations.isEmpty
 		
 		resultsController = {
 			let request: NSFetchRequest<Location> = Location.fetchRequest(in: map.region)
@@ -35,7 +52,27 @@ class ViewController: UIViewController {
 		assert(resultsController.fetchedObjects != nil)
 		
 		let locations = resultsController.fetchedObjects ?? []
-		let newlyInsertedLocations = Set(locations).subtracting(previousLocations).allObjects
+		
+		let shouldShowAnnotations: Bool = {
+			let $ = annotationVisibleThreshold
+			return map.camera.altitude < $.cameraDistance ||
+				locations.count < $.annotationCount
+		}()
+		guard shouldShowAnnotations else {
+			// TODO: Formatted localization
+			navigationItem.title = locations.count.description + NSLocalizedString("routers", comment: "When map camera latitude has not reached max visible annotations, shows navigation item title string")
+			navigationItem.prompt = NSLocalizedString("zoom in for details", comment: "When map camera latitude has not reached max visible annotations, shows navigation item prompt string")
+			navigationController?.setNavigationBarHidden(false, animated: true)
+			
+			map.removeAnnotations(map.annotations)
+			return
+		}
+		
+		navigationItem.title = nil
+		navigationItem.prompt = nil
+		navigationController?.setNavigationBarHidden(true, animated: true)
+		
+		let newlyInsertedLocations = didShowAnnotaions ? Set(locations).subtracting(previousLocations).allObjects : locations
 		let removedLocations = Set(previousLocations).subtracting(locations).allObjects
 		
 		map.removeAnnotations(removedLocations)
@@ -45,6 +82,8 @@ class ViewController: UIViewController {
 	// MARK: Life Cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		navigationController?.setNavigationBarHidden(true, animated: false)
 		
 		// Note: Request location asap to avoid map view loads nonrelated resources
 		func requestLocationAuthorization() {
